59a3fcab99d3eb649c520ef2887efa94d0195925
Wilson Liu <wliu5@unimelb.edu.au>
2017-04-06 22:43:12 +1000

    Adding test cases

---
+++ src/test/java/daris/ssh/client/test/SCPTest.java
@@ -0,0 +1,24 @@
+package daris.ssh.client.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SCPTest {
+
+    @Before
+    public void prepare() {
+        // TODO
+    }
+
+    @Test
+    public void test() {
+        // TODO
+    }
+
+    @After
+    public void cleanup() {
+        // TODO
+    }
+
+}
